f072020b18518fa166c729345b029870033ad4a8
Alex Stout <vickalexandra3@gmail.com>
2020-12-08 20:52:43 -0800

    stuff

---
+++ server/src/index.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-template-curly-in-string */
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+// eslint-disable-next-line import/no-unresolved
+const mongoose = require('mongoose');
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+require('dotenv').config();
+
+// eslint-disable-next-line import/no-unresolved
+const middlewares = require('./middlewares');
+const logs = require('./api/logs');
+
+const app = express();
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World!',
+  });
+});
+
+app.use('/api/logs', logs);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+const port = process.env.PORT || 1338;
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening at http://localhost:${port}');
+});
